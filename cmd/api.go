a0e5bb4302bc07f7db42423cd8a68651b3838fc1
HadeedTariq <computeranalog351@gmail.com>
2026-06-24 20:43:03 +0500

    setup production grade api of the goalng

---
+++ cmd/api.go
@@ -0,0 +1,10 @@
+package main
+
+type application struct {
+}
+
+type config struct{}
+
+func (app *application) mount() {}
+
+func (app *application) run() {}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/HadeedTariq/go-production-grade-api
+
+go 1.25.11
